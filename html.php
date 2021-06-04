9c22e6b685a73e5e79d28caead2518e6036e51eb
Raju Ahmed <71165334+raju1059@users.noreply.github.com>
2021-06-05 02:06:43 +0600

    Create html.php

---
+++ html.php
@@ -0,0 +1,15 @@
+<div class="form-group">
+				<label class="langBn control-label visible-ie8 visible-ie9">জেলা</label>
+				<label class="langEn control-label visible-ie8 visible-ie9">District</label>
+				<select id="districts" onchange="districtSelected()" class="form-control" style="text-align-last:center;">
+				
+				</select>
+			</div>
+			
+			<div class="form-group">
+				<label class="langBn control-label visible-ie8 visible-ie9">উপজেলা</label>
+				<label class="langEn control-label visible-ie8 visible-ie9">Upazela</label>
+				<select id="upazilas" name="upazilas" onchange="upazilaSelected()" class="form-control" style="text-align-last:center;">
+			
+				</select>
+			</div>
